a92b106c230f047a259b3e98aeb04d2d6dd2619c
Rahul.Kumar@wisetechglobal.com <Rahul.Kumar@wisetechglobal.com>
2022-06-04 10:42:35 +0530

    Add project files.

---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User.UserBuilder("Rahul", "Jaiswal")
+                .SetAge(10)
+                .SetPhone("8979813900")
+                .Build();
+
+            Console.WriteLine(user.GetFirstName() + " " + user.GetLastName());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BuilderPattern/User.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class User
+    {
+        private readonly string firstName;
+        private readonly string lastName;
+		private readonly int age;
+		private readonly string phone;
+		private readonly string address;
+
+		private User(UserBuilder builder)
+		{
+			firstName = builder.FirstName;
+			lastName = builder.LastName;
+			age = builder.Age;
+			phone = builder.Phone;
+			address = builder.Address;
+		}
+
+		public string GetFirstName()
+        {
+			return firstName;
+        }
+
+		public string GetLastName()
+		{
+			return lastName;
+		}
+
+		public int GetAge()
+		{
+			return age;
+		}
+		public string GetPhone()
+		{
+			return phone;
+		}
+		public string GetAddress()
+		{
+			return address;
+		}
+
+		public class UserBuilder
+		{
+			public string FirstName { get; private set; }
+			public string LastName { get; private set; }
+			public int Age { get; private set; }
+			public string Phone { get; private set; }
+			public string Address { get; private set; }
+
+			public UserBuilder(string firstName, string lastName)
+			{
+				FirstName = firstName;
+				LastName = lastName;
+			}
+			public UserBuilder SetAge(int age)
+			{
+				Age = age;
+				return this;
+			}
+			public UserBuilder SetPhone(string phone)
+			{
+				Phone = phone;
+				return this;
+			}
+			public UserBuilder SetAddress(string address)
+			{
+				Address = address;
+				return this;
+			}
+			public User Build()
+			{
+				User user = new User(this);
+				return user;
+			}
+		}
+	}
+}
